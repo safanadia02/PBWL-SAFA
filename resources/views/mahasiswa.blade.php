e2c3edb5c07492b7e84875ee1f6635644dbff689
safanadia02 <nadiabakri354@gmail.com>
2024-12-01 09:21:35 -0800

    Belajar membuat form didalam halaman mahasiswa

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -42,33 +42,70 @@
     <div class="container">
         <h1>Ini Adalah Halaman Mahasiswa</h1>
 
-        {{--
-            ROWSPAN = untuk menggabungkan baris
-            COLSPAN = untuk menggabungkan kolom
-            --}}
-
-        <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
-            <thead>
-            <tbody>
+       <div class="row">
+        <div class="col-sm-6">
+            <h4>Tabel Mahasiswa</h4>
+            <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+                <thead>
+                <tbody>
+                    <tr>
+                        <th>NPM</th>
+                        <th>Nama Mahasiswa</th>
+                        <th>Jenis kelamin</th>
+                        <th colspan="2">TTL</th>
+                    </tr>
+                </thead>
+                </tbody>
+                @for ($i = 0; $i < $jumlah; $i++)
                 <tr>
-                    <th>NPM</th>
-                    <th>Nama Mahasiswa</th>
-                    <th>Jenis kelamin</th>
-                    <th colspan="2">TTL</th>
-                </tr>
+                    <td>{{ $npm[$i] }}</td>
+                    <td>{{ $nama[$i] }}</td>
+                    <td>Perempuan</td>
+                    <td>Kota Medan</td>
+                </tr>                   
+                @endfor
             </thead>
-            </tbody>
-            @for ($i = 0; $i < $jumlah; $i++)
-            <tr>
-                <td>{{ $npm[$i] }}</td>
-                <td>{{ $nama[$i] }}</td>
-                <td>Perempuan</td>
-                <td>Kota Medan</td>
-            </tr>
+            </table>           
+        </div>
+        <div class="col-sm-6">
+            <h4>Form Mahasiswa</h4>
+            <form action="" method="GET">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label for="">NPM</label>
+                        <input type="number" name="npm" class="form-control" placeholder="Input NPM">
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="">Nama Mahasiswa</label>
+                        <input type="text" name="nama_mahasiswa" class="form-control" placeholder="Input Nama Mahasiswa">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <label for="">Tanggal Lahir</label>
+                        <input type="date" name="tgl_lahir" class="form-control">
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="">Prodi</label>
+                        <select name="prodi" class="form-control">
+                            <option value="1">Sistem Informasi</option>
+                            <option value="2">Teknik Informasi</option>
+                            <option value="3">Sains Data</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-sm-12">
+                     <button class="btn btn-primary"style="width: 100%" type="submit">Simpan</button>
+                    </div>
+                </div>
                 
-            @endfor
-        </thead>
-        </table>
+            </form>
+        </div>
+
+       </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
